195334f7b960de99a9c53ef666730d5c40c10e7f
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-14 13:59:40 -0200

    Update wsgi.py

--- siteFaculdade/wsgi.py
+++ siteFaculdade/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for siteFaculdadeV2 project.
+WSGI config for siteFaculdade project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteFaculdadeV2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteFaculdade.settings")
 
 application = get_wsgi_application()
